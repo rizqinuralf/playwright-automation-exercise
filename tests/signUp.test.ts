183d3f46cfbb4e7f4746acb7313ce0bb3d2b8dc9
rizqialfian <rizqi.alfian@qwiik.com>
2025-04-19 23:03:38 +0700

    add beforeEach scenario

--- tests/signUp.test.ts
+++ tests/signUp.test.ts
@@ -1,4 +1,4 @@
-import { test,expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { SignUpPage } from '../pages/signUpPage';
 import { LoginPage } from '../pages/loginPage';
 import { generateUser } from '../library/userCreation';
@@ -6,22 +6,31 @@ import { Dashboard } from '../pages/dashboardPage';
 
 test.describe.configure({ mode: 'serial' });
 test.describe('Running Test Cases from Automation Exercise', () => {
+  let loginPage: LoginPage;
+  let signUpPage: SignUpPage;
+  let dashboard: Dashboard;
+
+  const user = generateUser();
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    signUpPage = new SignUpPage(page);
+    dashboard = new Dashboard(page);
+
+  })
   test('Register User', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const signUpPage = new SignUpPage(page);
-    const dashboard = new Dashboard(page)
-    const user = generateUser();
-  
+
     await page.goto('/');
-    await expect(page.locator('img[alt="Website for automation practice"]')).toBeVisible();
-    await dashboard.loginButton.click()
+    await expect(
+      page.locator('img[alt="Website for automation practice"]')
+    ).toBeVisible();
+    await dashboard.loginButton.click();
     await loginPage.signUpAccess(user);
     await signUpPage.signUpProcess(user);
-    await dashboard.deleteAccountButton.click()
-    await expect(page.getByText('ACCOUNT DELETED!')).toBeVisible()
-    await signUpPage.continueButton.click()
-    }
-  );
+    await dashboard.deleteAccountButton.click();
+    await expect(page.getByText('ACCOUNT DELETED!')).toBeVisible();
+    await signUpPage.continueButton.click();
+  });
 
-})
 
+});
